4687e7d631b237a093522c9d0bcee1f4ec1f58c5
Tero Laxstrom <tlax@neonpeons.com>
2015-06-16 15:54:31 +0300

    Working through SSL
    Signed-off-by: Tero Laxstrom <tlax@neonpeons.com>

--- index.js
+++ index.js
@@ -1,22 +1,32 @@
+const crypto = require('crypto'),
+      fs = require('fs');
+
+var options = {
+    key: fs.readFileSync(__dirname + '/../example.com.key.pem'),
+    cert: fs.readFileSync(__dirname + '/../example.com.cert.pem')
+};
+
+var serverOptions = {
+    port: 3433
+};
+
 var app = require('express')();
-var server = require('http').createServer(app);
+var server = require('https').createServer(options, app);
 var io = require('socket.io').listen(server);
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/client/index.html');
 });
 
+app.get('/jquery.js', function(req, res){
+    res.sendFile(__dirname + '/client/jquery.js');
+});
+
 io.on('connection', function(socket){
     console.log('a user connected');
     
-    socket.on('chat message', function(msg){
-        io.emit('chat message', msg);
-        if(msg[0] === '/') {
-            console.log('command: ' + msg);
-            console.log(socket);
-        } else {
-            console.log('message: ' + msg);
-        }
+    socket.on('join', function (name) {
+        console.log(name + ' joined');
     });
     
     socket.on('disconnect', function() {
@@ -24,6 +34,6 @@ io.on('connection', function(socket){
     });
 });
 
-server.listen(3000, function(){
-    console.log('listening on *:3000');
+server.listen(serverOptions.port, function() {
+    console.log("listening on *:" + serverOptions.port);
 });
