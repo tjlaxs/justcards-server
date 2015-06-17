a03b55ff25c44b8039a5615344b3242235032c43
Tero Laxstrom <tlax@neonpeons.com>
2015-06-17 19:34:19 +0300

    Moved hashing to a common.js file and added new tables to db creation script.
    Signed-off-by: Tero Laxstrom <tlax@neonpeons.com>

--- create_db.js
+++ create_db.js
@@ -1,24 +1,42 @@
-var crypto = require('crypto');
-var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database('sqlite3.db');
-var globalSalt="DippadapppaHobbabobbaLiulauloiasdfjotiantekstiivaase8";
+/* Notice: requires hashData(pw,salt) function */
 
-function hashPassword(password, salt) {
-    var hash = crypto.createHash('sha256');
-    hash.update(password);
-    hash.update(salt);
-    return hash.digest('hex');
-};
+function JCcreateDB() {
+    var common = require('./js/common.js');
+    var sqlite3 = require('sqlite3').verbose();
+    var db = new sqlite3.Database('sqlite3.db');
+    var globalSalt="DippadapppaHobbabobbaLiulauloiasdfjotiantekstiivaase8";
 
-db.serialize(function () {
-    db.run("CREATE TABLE if not exists users (Id INTEGER PRIMARY KEY, Nickname TEXT, Password TEXT, Superuser INTEGER DEFAULT 0);");
-    var stmt = db.prepare("INSERT INTO users(Nickname, Password, Superuser) VALUES (?, ?, ?)");
-    stmt.run('Admin', hashPassword('Admin', globalSalt), 1);
-    stmt.finalize();
-    
-    db.each("SELECT * from users", function (err, row) {
-        console.log(row.Id + ": " + row.Nickname + "(" + row.Password + ")");
+    db.serialize(function () {
+        /* Users */
+        db.run("CREATE TABLE if not exists User (Id INTEGER PRIMARY KEY, Nickname TEXT, Password TEXT, Superuser INTEGER DEFAULT 0);");
+        var stmt = db.prepare("INSERT INTO User(Nickname, Password, SuperUser) VALUES (?, ?, ?);");
+        stmt.run('Admin', common.hashData('Admin', globalSalt), 1);
+        stmt.finalize();
+        
+        /* Images */
+        db.run("CREATE TABLE if not exists Image (Id INTEGER PRIMARY KEY, Name TEXT, Path TEXT, Owner INTEGER, FOREIGN KEY(Owner) REFERENCES User(Id));");
+        
+        /* Decks */
+        db.run("CREATE TABLE if not exists Deck (Id INTEGER PRIMARY KEY, AddTime TEXT, Name TEXT, CardBack INTEGER, Owner INTEGER, FOREIGN KEY(CardBack) REFERENCES Image(Id), FOREIGN KEY(Owner) REFERENCES User(Id));");
+        
+        /* Cards */
+        db.run("CREATE TABLE if not exists Card (Id INTEGER PRIMARY KEY, CardFace INTEGER, FOREIGN KEY(CardFace) REFERENCES Image(Id));");
+        
+        /* Events */
+        db.run("CREATE TABLE if not exists Event (Id INTEGER PRIMARY KEY, Time TEXT, Owner INTEGER);");
+        db.run("CREATE TABLE if not exists AddCardEvent (Id INTEGER PRIMARY KEY, Image INTEGER, User INTEGER, FOREIGN KEY(Image) REFERENCES Image(Id), FOREIGN KEY (User) REFERENCES User(Id));");
+
+        /* Tell what we have done */
+        db.each("SELECT * from User", function (err, row) {
+            console.log("User " + row.Id + ": " + row.Nickname + "(" + row.Password + ")");
+        });
+        var i = 1;
+        db.each("SELECT name FROM sqlite_master WHERE type IN ('table', 'view') AND name NOT LIKE 'sqlite_%';", function (err, row) {
+            console.log("Table " + i++ + ": " + row.name);
+        });
     });
-});
 
-db.close();
+    db.close();
+}
+
+JCcreateDB();
